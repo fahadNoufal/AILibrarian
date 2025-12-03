cfe3f302a1f8ac97c5b2bb54a7a4136eec0490be
Fahad <fhdnaufal@gmail.com>
2025-12-03 10:51:19 +0530

    structure- created

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md","r",encoding='utf-8') as f:
+    description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = 'AILibrarian'
+AUTHOR_USER_NAME ='fahadNoufal'
+SRC_REPO = 'AILibrarian'
+AUTHOR_EMAIL = 'fhdnaufal@gmail.com'
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Machine learning book recommendation engine.",
+    long_description=description,
+    long_description_content_type='text/markdown',
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+)
